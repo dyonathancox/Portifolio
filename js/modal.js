6ddce3a3d453ffe8af74d0fc50b3e392b2d095ec
Dyonathan Cox <86791327+dyonathancox@users.noreply.github.com>
2024-10-29 23:30:07 -0300

    update modal

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,25 @@
-
-// criando a função do modal de projetos
+// Criando a função do modal de projetos
 export function openModal(modalId) {
     document.getElementById(modalId).style.display = "block";
+
+    // evento para fechar o modal com o ESC
+    document.addEventListener("keydown", handleEscKey);
 }
 
 export function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
+
+    document.removeEventListener("keydown", handleEscKey);
+}
+
+//verificação se ESC foi pressionada
+function handleEscKey(event) {
+    if (event.key === "Escape") {
+        const modal = document.querySelector(".modal[style*='display: block']");
+        if (modal) {
+            closeModal(modal.id);
+        }
+    }
 }
 
 window.openModal = openModal;
